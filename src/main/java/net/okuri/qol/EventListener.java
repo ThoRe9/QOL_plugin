96ffae79cfa9d6b5509267a0bbf7a520befe332b
RIKU <ikeda.1818@outlook.jp>
2023-09-10 18:09:10 +0900

    alcohol system

--- src/main/java/net/okuri/qol/EventListener.java
+++ src/main/java/net/okuri/qol/EventListener.java
@@ -116,23 +116,53 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    // 材料ポーションなど使用できないアイテムを使用したときの処理
     public void PlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
         ItemMeta meta = item.getItemMeta();
-        NamespacedKey key = new NamespacedKey("qol", "qol_consumable");
+        // 材料ポーションなど使用できないアイテムを使用したときの処理
+        this.unconsumableEvent(event, player, meta);
 
+        // ストゼロの処理
+        this.St0Event(player, item, meta);
+
+        // alcoholを使用したとき
+        this.alcoholEvent(player, meta);
+
+    }
+    private void alcoholEvent(Player player, ItemMeta meta){
+        // metaのPersistentDataContainerにalcoholKeyがあるか確認
+        if (!meta.getPersistentDataContainer().has(Alcohol.alcKey, PersistentDataType.BOOLEAN)){
+            return;
+        }
+        if (!meta.getPersistentDataContainer().get(Alcohol.alcKey, PersistentDataType.BOOLEAN)){
+            return;
+        }
+        // プレイヤーのAlcoholLevelを取得
+        // ない場合は作成
+        if (!player.getPersistentDataContainer().has(Alcohol.alcLvKey, PersistentDataType.DOUBLE)){
+            player.getPersistentDataContainer().set(Alcohol.alcLvKey, PersistentDataType.DOUBLE, 0.00);
+        }
+        double alcLv = player.getPersistentDataContainer().get(Alcohol.alcLvKey, PersistentDataType.DOUBLE);
+        // itemのalcAmount * alcPerをalcLvに加算
+        double alcAmount = meta.getPersistentDataContainer().get(Alcohol.alcAmountKey, PersistentDataType.DOUBLE);
+        double alcPer = meta.getPersistentDataContainer().get(Alcohol.alcPerKey, PersistentDataType.DOUBLE);
+        alcLv += alcAmount * alcPer;
+        player.getPersistentDataContainer().set(Alcohol.alcLvKey, PersistentDataType.DOUBLE, alcLv);
+        // alcoholの効果を与える
+        new Alcohol().run();
+    }
+    private void unconsumableEvent(PlayerItemConsumeEvent event, Player player, ItemMeta meta){
+        NamespacedKey key = new NamespacedKey("qol", "qol_consumable");
         if (meta.getPersistentDataContainer().has(key, PersistentDataType.BOOLEAN)){
             if (!meta.getPersistentDataContainer().get(key, PersistentDataType.BOOLEAN)){
                 new ChatGenerator().addWarning("You cannot use it!").sendMessage(player);
                 event.setCancelled(true);
             }
         }
-
-        // ストゼロの処理
-        this.St0Event(player, item, meta);
     }
+
+
     private void St0Event(Player player, ItemStack item, ItemMeta meta){
         // ストゼロの効果
         if (meta.getPersistentDataContainer().has(StrongZero.st0key, PersistentDataType.STRING)){
