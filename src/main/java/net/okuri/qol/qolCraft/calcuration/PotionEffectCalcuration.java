12a7513aa02725884b6dada7bf920419f06420d4
RIKU <ikeda.1818@outlook.jp>
2023-09-18 00:35:38 +0900

    SuperWheatのパラメータ計算の移植

---
+++ src/main/java/net/okuri/qol/qolCraft/calcuration/PotionEffectCalcuration.java
@@ -0,0 +1,28 @@
+package net.okuri.qol.qolCraft.calcuration;
+
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+
+public abstract class PotionEffectCalcuration implements Calcuration{
+    protected ArrayList<PotionEffect> potionEffects;
+    public ArrayList<PotionEffect> getPotionEffects(){
+        return this.potionEffects;
+    }
+    public void setEffectType(int index, PotionEffectType effectType){
+        potionEffects.set(index, new PotionEffect(effectType, 0, 0));
+    }
+
+    @Override
+    public ArrayList<Double> getAns(){
+        ArrayList<Double> ans = new ArrayList<>();
+        for (PotionEffect potionEffect : potionEffects) {
+            ans.add((double) potionEffect.getDuration());
+            ans.add((double) potionEffect.getAmplifier());
+        }
+        return ans;
+    }
+
+
+}
---
+++ src/main/java/net/okuri/qol/qolCraft/calcuration/RandFromXYZ.java
@@ -0,0 +1,43 @@
+package net.okuri.qol.qolCraft.calcuration;
+
+import org.bukkit.Bukkit;
+
+import java.util.ArrayList;
+
+public class RandFromXYZ implements Calcuration{
+    private int x;
+    private int y;
+    private int z;
+    private int divLine;
+    private double scale;
+    private ArrayList<Double> answer = new ArrayList<>();
+    @Override
+    public void setVariable(double... variable){
+        if (variable.length != 5) {
+            throw new IllegalArgumentException("Variables are not long enough.");
+        }
+        this.x = (int) variable[0];
+        this.y = (int) variable[1];
+        this.z = (int) variable[2];
+        this.divLine = (int) variable[3];
+        this.scale =  variable[4];
+    }
+
+    @Override
+    public void calcuration(){
+        answer = new ArrayList<>();
+        double dx = (((double)(Math.abs(x+y))%divLine) / divLine)*scale;
+        double dy = (((double)(Math.abs(y+z))%divLine) / divLine)*scale;
+        double dz = (((double)(Math.abs(z+x))%divLine) / divLine)*scale;
+        answer.add(dx - dz);
+        answer.add(dy - dx);
+        answer.add(dz - dy);
+        Bukkit.getLogger().info("dx: " + dx + " dy: " + dy + " dz: " + dz);
+    }
+
+    @Override
+    public ArrayList<Double> getAns(){
+        return this.answer;
+    }
+}
+
